ecd2334f42ecc891b5b148606d0c50247b96b123
Jose Torres <vidojam@gmail.com>
2023-06-15 22:06:55 -0400

    Contact page fix

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Image } from "react-bootstrap";
 import Layout from "../layout/AppLayout.jsx";
 
 const ContactForm = () => {
@@ -11,11 +11,7 @@ const ContactForm = () => {
     e.preventDefault();
     // Perform the necessary logic to send the email or process the form data
     // You can use a library like axios to make an API request to a server-side endpoint
-
-    // Example code to demonstrate the form data
-    console.log('Name:', name);
-    console.log('Email:', email);
-    console.log('Message:', message);
+    
 
     // Reset the form fields
     setName('');
@@ -27,43 +23,55 @@ const ContactForm = () => {
     <Layout>
       <Container className="contact">
         <Row>
-        <Col>
-        <form onSubmit={handleSubmit}>
-        <h1 className="emailMeTitle">Email Me</h1>
-          <div>
-            <label htmlFor="name">Name:</label>
-            <input
-              type="text"
-              id="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="message">Message:</label>
-            <textarea
-              id="message"
-              rows="15"
-              cols="21"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              required>
-            </textarea>
-          </div>
-          <button id="submit" type="submit">Submit</button>
-        </form>
-        </Col>
+        <Image fluid
+                  className="d-block w-25"
+                  src="/images/email2.jpeg"
+                  alt="Email icon"
+                />
+          <Col>
+            <form onSubmit={handleSubmit}>
+            <h1 className="emailMeTitle">Email Me, For Free</h1>
+              <div>
+                <label className="name" htmlFor="name">Name:</label>
+                <input
+                  type="text"
+                  id="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
+              </div>
+              <div>
+                <label className="email" htmlFor="email">Email:</label>
+                <input
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div>
+                <label className="message" htmlFor="message">Message:</label>
+                <textarea
+                  id="message"
+                  rows="15"
+                  cols="21"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  required>
+                </textarea>
+              </div>
+              <button id="submit" type="submit">Submit</button>
+            </form>
+          </Col>
+          <div className="emailImg">
+            <Image fluid
+                    className="d-block w-25"
+                    //src="/images/email2.jpeg"
+                    //alt="Email icon"
+                  />
+          </div>      
         </Row>
       </Container>
     </Layout>
